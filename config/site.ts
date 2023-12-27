3653f714c793652c6fad6f5d91bb8a89a0f4da1e
tarun268 <tarun@prodios.com>
2023-12-27 13:25:30 +0530

    feat(auth): integrate clerk authentication

---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: 'Next-Board',
+  description: 'Elevate productivity and collaboration with Next-Board, a streamlined task management tool for teams.',
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { siteConfig } from '../../config/site'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Next-Board',
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
   description: 'Next-Board is a task management tool for teams.',
 }
 
