40a9eb1b6fbcaa463b43f13fc294766bc345bcac
tarun268 <tarun@prodios.com>
2023-12-28 15:15:07 +0530

    feat(sidebar): add workspaces sidebar

--- src/app/(platform)/(dashboard)/_components/navbar.tsx
+++ src/app/(platform)/(dashboard)/_components/navbar.tsx
@@ -3,10 +3,12 @@ import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
 
 import { Logo } from '@/components/logo'
 import { Button } from '@/components/ui/button'
+import { MobileSidebar } from './mobile-sidebar'
 
 export const Navbar = () => {
   return (
     <nav className="fixed top-0 z-50 flex h-14 w-full items-center border-b bg-white px-4 shadow-sm">
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
