098445573c1e3dd7f2f04ead1292fed33ff30d46
tarun268 <tarun@prodios.com>
2024-01-29 17:28:20 +0530

    feat(pro-modal): add pro modal

--- src/components/form/form-popover.tsx
+++ src/components/form/form-popover.tsx
@@ -11,6 +11,7 @@ import { createBoard } from '@/actions/create-board'
 import { FormInput } from './form-input'
 import { FormSubmit } from './form-submit'
 import { FormPicker } from './form-picker'
+import { useProModal } from '@/hooks/use-pro-modal'
 
 interface FormPopoverProps {
   children: React.ReactNode
@@ -20,6 +21,7 @@ interface FormPopoverProps {
 }
 
 export const FormPopover = ({ children, side = 'bottom', align, sideOffset = 0 }: FormPopoverProps) => {
+  const proModal = useProModal()
   const router = useRouter()
   const closeRef = useRef<ElementRef<'button'>>(null)
 
@@ -31,6 +33,7 @@ export const FormPopover = ({ children, side = 'bottom', align, sideOffset = 0 }
     },
     onError: (error) => {
       toast.error(error)
+      proModal.onOpen()
     },
   })
 
